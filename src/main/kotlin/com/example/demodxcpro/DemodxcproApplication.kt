425d2cf764097c2dc26b6826ee2b928b4329353e
sandeep <sandeepreddy4366@gmail.com>
2021-11-23 22:26:56 +0530

    initial commit

---
+++ src/main/kotlin/com/example/demodxcpro/DemodxcproApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demodxcpro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemodxcproApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemodxcproApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demodxcpro/DemodxcproApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demodxcpro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemodxcproApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
